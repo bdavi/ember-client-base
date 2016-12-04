fff1c7de4e775bff0011ed4312da5af1a5c55f93
Brian Davies <brian@briandavies.me>
2016-12-04 16:58:48 -0700

    Add user model

---
+++ app/models/user.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+const { attr } = DS;
+
+export default DS.Model.extend({
+
+  name: attr('string'),
+
+  password: attr('string'),
+
+  email: attr('string'),
+
+});
