de0dfc624792e22420084ca3c5028a1e64274ea4
Brian Davies <brian@briandavies.me>
2016-12-04 17:09:28 -0700

    Allow user logout

--- app/components/nav-top-user-menu.js
+++ app/components/nav-top-user-menu.js
@@ -1,11 +1,21 @@
 import Ember from 'ember';
 
+const { inject } = Ember;
+
 export default Ember.Component.extend({
+
+  session: inject.service('session'),
 
   tagName: 'li',
 
   classNames: ['dropdown'],
 
   user: null,
+
+  actions: {
+   logout() {
+      this.get('session').invalidate();
+    }
+  }
 
 });
